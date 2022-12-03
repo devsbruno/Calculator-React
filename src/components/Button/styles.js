88d69f37a98fb571709f4458557c9d05897cac8b
devsbruno <brunoalves.devs@gmail.com>
2022-12-03 01:18:55 -0300

    First Commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #050030;
+    background-color: #444444;
+    color: #22ff22;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+        border: 1px solid #cdcdcd;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 150px;
+    background-color: #050505;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #050505;
+        border: 0;
+        text-align: right;
+
+        padding: 0 10px;
+        font-size: 84px;
+        font-family: 'Roboto';
+        color: #22ff22;
+    }
+`
